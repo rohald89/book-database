a6440248ad05525a3416e3ceb829b9c923d9a57c
Rohald van Merode <rohald_89@live.nl>
2021-10-14 19:58:52 +0200

    added database model

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-# import models
+from models import (Base, session, 
+                    Book, engine)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
 # main menu - add, search, analysis, exit, View
 # add books to the database
 # edit books
